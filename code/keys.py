bdd6d340cbffb239041032751b509afb460ddadb
Kai Wells <support@kaiwells.me>
2021-05-05 09:49:18 -0700

    Customize alphabet

--- code/keys.py
+++ code/keys.py
@@ -3,7 +3,7 @@ from typing import Set
 from talon import Module, Context, actions, app
 import sys
 
-default_alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split(
+default_alphabet = "air bat cap drum each fine gust hat eek jam crunch look man near odd pit quench red sun trap urge vest well plex yank zip".split(
     " "
 )
 letters_string = "abcdefghijklmnopqrstuvwxyz"
